6c0bf8310448fcef5f62a382cc24dc764c5ea20e
RyanJ119 <61523480+RyanJ119@users.noreply.github.com>
2022-04-26 17:22:57 -0400

    Update sensitivitySpecificity.py

--- sensitivitySpecificity.py
+++ sensitivitySpecificity.py
@@ -3,13 +3,14 @@ def sensitivitySpecificity(mat, fold):
     """
     Input: a confusion matrix and a fold(scalar)
     
-    Output: truePositive, trueNegative,falsePositive,falseNegative rates
+    Output: Sensitivity and Specificity values for the given fold
     """
-
+    # initialize values for computing Sensitivity and Specificity
     truePositive = mat[fold][fold]
     trueNegative = 0
     falsePositive = 0
     falseNegative = 0
+    #calculate true negative, true positive, false negative and false positive values
     for i in range(len(mat)):
         for j in range(len(mat)):
             if i != j and i == fold:
@@ -17,8 +18,8 @@ def sensitivitySpecificity(mat, fold):
             if i != j and j == fold:
                 falseNegative = falseNegative+mat[i][j]
             if i!=fold and j!=fold:
-                
                 trueNegative= trueNegative+mat[i][j]
+     # Calculate Sensitivity and Specificity        
     sensitivity =      truePositive / (truePositive+falseNegative)  
     specificity =    trueNegative / (trueNegative + falsePositive)   
                 
@@ -32,17 +33,22 @@ def printSpecifictySensitivity(mat):
     
     Output: Prints sensitivity and specificity in order of the columns of the confusion matrix
     """
+    # initialize collection list
     vals = []
+    #run sensitivitySpecificity function on each of the labels in order, collecting them in vals
     for i in range(len(mat)):
         vals.append( sensitivitySpecificity(mat, i)) 
 #        print("the sensitivity of fold {} is {}".format(i+1, vals[0]))
 #        print("the specificity of fold {} is {}".format(i+1, vals[1]))
 #    for i in vals:
 #        print(i)
+
     print("sensitivity for folds in order:")
+    #Print all sensitivity values in order of confusion matrix columns 
     for i in vals:
         print("{:.2f}".format(i[0]), end = ' , ')
     print()
+    #print all specificity values in order of confusion matrix columns
     print("specificity for folds in order:")
     for i in vals:
         print("{:.2f}".format(i[1]),  end = ' , ')
